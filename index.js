af2303f488f89433248d513084dfad7c579da371
mohin556 <mohinalhaq@gmail.com>
2023-08-28 21:19:43 +0600

    single-server

---
+++ index.js
@@ -0,0 +1,117 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+// midleware 
+
+app.use(cors());
+app.use(express.json());
+
+
+
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2iwiq.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const productCollection = client.db('productDB').collection('productItm');
+    const sectorsCollection = client.db('taskdb').collection('worktyp');
+    const detailCollection = client.db('taskdb').collection('detailss');
+   
+    app.get('/sectors', async(req,res)=>{
+      const cursor = sectorsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+
+   })
+   
+
+
+
+   
+    app.get('/product', async(req,res)=>{
+       const cursor = productCollection.find();
+       const result = await cursor.toArray();
+       res.send(result)
+    })
+
+
+
+    app.post('/product',async(req,res) =>{
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    })
+ 
+    app.post('/details',async(req,res) =>{
+      const product = req.body;
+      const result = await detailCollection .insertOne(product);
+      res.send(result);
+    })
+
+    app.get('/details', async(req,res)=>{
+      const cursor = detailCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+
+   })
+
+
+   app.delete('/details/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}   
+      const result = await detailCollection.deleteOne(query);
+      res.send(result)
+   })
+
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Running my website server")
+})
+
+app.listen(port, () =>{
+    console.log(` server is running on ${port}`)
+})
